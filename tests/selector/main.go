9f22f6851f8306b0bbcf896f73eb39ead05c78b2
Przemek Delewski <pdelewski@sumologic.com>
2022-08-16 15:31:55 +0200

    removing instrumentation code

--- tests/selector/main.go
+++ tests/selector/main.go
@@ -2,40 +2,25 @@ package main
 
 import (
 	"fmt"
-	"context"
 
 	"github.com/pdelewski/autotel/rtlib"
-	otel "go.opentelemetry.io/otel"
 )
 
 type Driver struct {
 	value int
 }
 
-func (d Driver) foo(__tracing_ctx context.Context, i int) Driver {
-	__child_tracing_ctx, span := otel.Tracer("foo").Start(__tracing_ctx, "foo")
-	_ = __child_tracing_ctx
-	defer span.End()
+func (d Driver) foo(i int) Driver {
 	return Driver{i}
 }
 
 func (d Driver) bar(i int) Driver {
-	__child_tracing_ctx := context.TODO()
-	_ = __child_tracing_ctx
 	return Driver{d.value + i}
 }
 
 func main() {
-	__child_tracing_ctx := context.TODO()
-	_ = __child_tracing_ctx
-	ts := rtlib.NewTracingState()
-	defer rtlib.Shutdown(ts)
-	otel.SetTracerProvider(ts.Tp)
-	ctx := context.Background()
-	__child_tracing_ctx, span := otel.Tracer("main").Start(ctx, "main")
-	defer span.End()
 	rtlib.AutotelEntryPoint__()
 	d := Driver{0}
-	d = d.foo(__child_tracing_ctx, 4).bar(5)
+	d = d.foo(4).bar(5)
 	fmt.Println(d.value)
 }
