aae717a2ddcd48575535a3f3a0ab3107fd7f8e71
Przemek Delewski <pdelewski@sumologic.com>
2022-08-17 11:16:25 +0200

    recursion expectations updated

--- tests/expected/recursion/recursion.go_pass_tracing.go_pass_ctx.go
+++ tests/expected/recursion/recursion.go_pass_tracing.go_pass_ctx.go
@@ -16,16 +16,28 @@ package main
 
 import (
 	"github.com/pdelewski/autotel/rtlib"
+	otel "go.opentelemetry.io/otel"
 	"context"
 )
 
 func recur(__tracing_ctx context.Context, n int) {
+	__child_tracing_ctx, span := otel.Tracer("recur").Start(__tracing_ctx, "recur")
+	_ = __child_tracing_ctx
+	defer span.End()
 	if n > 0 {
 		recur(__child_tracing_ctx, n-1)
 	}
 }
 
 func main() {
+	__child_tracing_ctx := context.TODO()
+	_ = __child_tracing_ctx
+	ts := rtlib.NewTracingState()
+	defer rtlib.Shutdown(ts)
+	otel.SetTracerProvider(ts.Tp)
+	ctx := context.Background()
+	__child_tracing_ctx, span := otel.Tracer("main").Start(ctx, "main")
+	defer span.End()
 	rtlib.AutotelEntryPoint__()
 	recur(__child_tracing_ctx, 5)
 }
