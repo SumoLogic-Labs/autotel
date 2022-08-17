8e9de384caa381f69954774abe984c94effc433b
Przemek Delewski <pdelewski@sumologic.com>
2022-08-17 11:06:04 +0200

    methods expectations updated

--- tests/expected/methods/main.go_pass_tracing.go_pass_ctx.go
+++ tests/expected/methods/main.go_pass_tracing.go_pass_ctx.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"github.com/pdelewski/autotel/rtlib"
+	otel "go.opentelemetry.io/otel"
 	"context"
 )
 
@@ -34,20 +35,41 @@ type impl struct {
 }
 
 func (i impl) foo(__tracing_ctx context.Context, p int) int {
+	__child_tracing_ctx, span := otel.Tracer("foo").Start(__tracing_ctx, "foo")
+	_ = __child_tracing_ctx
+	defer span.End()
 	return 5
 }
 
-func foo(__tracing_ctx context.Context, p int) int {
+func foo(p int) int {
+	__child_tracing_ctx := context.TODO()
+	_ = __child_tracing_ctx
 	return 1
 }
 
 func (d driver) process(__tracing_ctx context.Context, a int) {
+	__child_tracing_ctx, span := otel.Tracer("process").Start(__tracing_ctx, "process")
+	_ = __child_tracing_ctx
+	defer span.End()
 }
 
 func (e element) get(__tracing_ctx context.Context, a int) {
+	__child_tracing_ctx, span := otel.Tracer("get").Start(__tracing_ctx, "get")
+	_ = __child_tracing_ctx
+	defer span.End()
+
 }
 
 func main() {
+	__child_tracing_ctx := context.TODO()
+	_ = __child_tracing_ctx
+	ts := rtlib.NewTracingState()
+	defer rtlib.Shutdown(ts)
+	otel.SetTracerProvider(ts.Tp)
+	ctx := context.Background()
+	__child_tracing_ctx, span := otel.Tracer("main").Start(ctx, "main")
+	defer span.End()
+
 	rtlib.AutotelEntryPoint__()
 	d := driver{}
 	d.process(__child_tracing_ctx, 10)
