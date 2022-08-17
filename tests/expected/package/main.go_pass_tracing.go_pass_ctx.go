ff54eba38325b3610020be4bb78c273e9d0680f7
Przemek Delewski <pdelewski@sumologic.com>
2022-08-17 11:12:18 +0200

    package expectations updated

--- tests/expected/package/main.go_pass_tracing.go_pass_ctx.go
+++ tests/expected/package/main.go_pass_tracing.go_pass_ctx.go
@@ -16,15 +16,27 @@ package main
 
 import (
 	"os"
+	"context"
 
 	"github.com/pdelewski/autotel/rtlib"
+	otel "go.opentelemetry.io/otel"
 )
 
 func Close() error {
+	__child_tracing_ctx := context.TODO()
+	_ = __child_tracing_ctx
 	return nil
 }
 
 func main() {
+	__child_tracing_ctx := context.TODO()
+	_ = __child_tracing_ctx
+	ts := rtlib.NewTracingState()
+	defer rtlib.Shutdown(ts)
+	otel.SetTracerProvider(ts.Tp)
+	ctx := context.Background()
+	__child_tracing_ctx, span := otel.Tracer("main").Start(ctx, "main")
+	defer span.End()
 	rtlib.AutotelEntryPoint__()
 	f, e := os.Create("temp")
 	defer f.Close()
