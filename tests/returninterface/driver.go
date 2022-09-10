0c412819a23dca34ad1a61af2aa7b97dd27f2cbb
Przemek Delewski <pdelewski@sumologic.com>
2022-09-10 21:32:35 +0200

    return via interface

---
+++ tests/returninterface/driver.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/pdelewski/autotel/rtlib"
+
+type Serializer interface {
+	Serialize()
+}
+
+type BasicSerializer struct {
+}
+
+func (s BasicSerializer) Serialize() {
+
+}
+
+func MakeSerializer() Serializer {
+	s := BasicSerializer{}
+	return s
+}
+
+func main() {
+	rtlib.AutotelEntryPoint__()
+	s := MakeSerializer()
+	s.Serialize()
+}
