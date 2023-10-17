f11b8274e3c24f57e12e2cc2d6460671c2fc288e
Przemek Delewski <pdelewski@sumologic.com>
2023-10-17 15:26:23 +0200

    update imports

--- driver/log2traces/imports.go
+++ driver/log2traces/imports.go
@@ -4,6 +4,7 @@ import (
     _ "go.opentelemetry.io/contrib/instrgen/rtlib"
     _ "go.opentelemetry.io/otel"
     _ "context"
+    _ "runtime"
     _ "go.opentelemetry.io/otel/trace"
     _ "go.opentelemetry.io/otel/sdk/trace"
 )
---
+++ driver/log2traces/util/imports.go
@@ -0,0 +1,10 @@
+package util
+
+import (
+	_ "go.opentelemetry.io/contrib/instrgen/rtlib"
+	_ "go.opentelemetry.io/otel"
+	_ "context"
+	_ "runtime"
+	_ "go.opentelemetry.io/otel/trace"
+	_ "go.opentelemetry.io/otel/sdk/trace"
+)
